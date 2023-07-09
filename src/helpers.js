df1e14b3dd236794dd5a1f09e9ae5f2b0f99c452
Aman Upadhyay <amazeballsaman@gmail.com>
2023-07-09 19:35:54 +0530

    updated the Adding expenses form and fixed the bug

--- src/helpers.js
+++ src/helpers.js
@@ -27,6 +27,20 @@ export const createBudget = ({name,amount}) => {
 }
 
 
+// create expense
+export const createExpense = ({name,amount, budgetId}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId : budgetId
+  }
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem("expenses" , JSON.stringify([...existingExpenses , newItem]))
+}
+
+
  // delete item
  export const deleteItem = ({key}) => {
    return localStorage.removeItem(key);
@@ -48,6 +62,16 @@ export const createBudget = ({name,amount}) => {
 
 
  // Formatting
+
+
+// Format Percentages
+export const formatPercentage = (amt) => {
+return amt.toLocaleString(undefined, {
+  style : "percent",
+  minimumFractionDigits : 0,  
+})
+}
+
 
  // Format Currency
 export const formatCurrency = (amt) => {
